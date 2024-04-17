c645423a02fe9abc314622bb6ed4a232aaacf522
JezzDiego <oliveirajesse50@gmail.com>
2024-04-17 19:41:36 -0300

    update user model

--- models/login.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Login struct {
-	Id       string `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Salt     string `json:"salt"`
-}
--- models/user.go
+++ models/user.go
@@ -5,7 +5,10 @@ import (
 )
 
 type User struct {
+	Id          string    `json:"id"`
 	Name        string    `json:"name"`
+	Email       string    `json:"email"`
+	Password    string    `json:"password"`
 	CPF         string    `json:"cpf"`
 	BirthDate   time.Time `json:"birthDate"`
 	PhoneNumber []string  `json:"phoneNumber"`
